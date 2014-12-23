dc643c599754f37e44473f8d3f0527e3ee276a4f
Ivonei Junior <ivoneijr@gmail.com>
2014-12-23 17:26:28 -0200

    dashboard_id serialized

--- app/serializers/widget_serializer.rb
+++ app/serializers/widget_serializer.rb
@@ -1,5 +1,10 @@
 class WidgetSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :color, :position, :size
+  attributes :id, :name, :description, :color, :position, :size, :dashboard_id
   has_one :indicator
   has_one :widget_type
+
+  def dashboard_id
+    object.dashboard.id
+  end
+  
 end

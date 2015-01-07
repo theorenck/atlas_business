0b852f35eb5d517114f6aa7092536c71ce15b817
Theo Renck <carlosrenck@gmail.com>
2015-01-07 14:42:51 -0200

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -16,4 +16,4 @@
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-AtlasBussiness::Application.config.secret_token = 'a9549949b1fb9144cd55242b803e8e9a3d6c1a5c47e890fc8f35758b75e05588c5b2cc768b9736d508ef882bc2380039e7402484d6b746804c27b8282087a66b'
+AtlasBusiness::Application.config.secret_token = 'a9549949b1fb9144cd55242b803e8e9a3d6c1a5c47e890fc8f35758b75e05588c5b2cc768b9736d508ef882bc2380039e7402484d6b746804c27b8282087a66b'

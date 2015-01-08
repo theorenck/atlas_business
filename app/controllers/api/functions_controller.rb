e116658e53bfc48a4c21d99473d3ec2e59c5b2cb
Ivonei Junior <ivoneijr@gmail.com>
2015-01-08 18:32:08 -0200

    functions controller updated

--- app/controllers/api/functions_controller.rb
+++ app/controllers/api/functions_controller.rb
@@ -1,6 +1,9 @@
 class API::FunctionsController < ApplicationController
   
-
+  # GET /functions/1
+  def show
+    render json: @function
+  end
   # GET /functions
   def index
     @functions = Function.all

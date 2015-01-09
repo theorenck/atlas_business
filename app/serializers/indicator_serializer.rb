debb5bc474c83b8f0649e28c97dec9791ad2cac7
Ivonei Junior <ivoneijr@gmail.com>
2015-01-09 16:22:35 -0200

    Indicator serializer refactor for unity_id and source_id

--- app/serializers/indicator_serializer.rb
+++ app/serializers/indicator_serializer.rb
@@ -1,6 +1,14 @@
 class IndicatorSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :code
+  attributes :id, :name, :description, :code, :unity_id, :source_id
 
-  has_one :source
-  has_one :unity
+  # has_one :source
+  # has_one :unity
+
+  def unity_id
+    object.unity_id
+  end
+
+  def source_id
+    object.source_id
+  end
 end
